305e9327fb0de792dffd3ab8e15887ba89164d64
Sky Bristol <sbristol@usgs.gov>
2017-06-27 08:14:12 -0600

    Fixes to the sgcn and worms modules

--- bis/sgcn.py
+++ bis/sgcn.py
@@ -12,7 +12,18 @@ def clearStateYear(baseURL,sgcn_state,sgcn_year):
 # Insert a record into the SGCN table
 def insertSGCNData(baseURL,record):
     import requests
-    q = "INSERT INTO sgcn.sgcn (sourceid,sourcefilename,sourcefileurl,sgcn_state,sgcn_year,scientificname_submitted,commonname_submitted,taxonomicgroup_submitted,firstyear) \
+    from datetime import datetime
+
+    q = "INSERT INTO sgcn.sgcn (sourceid,sourcefilename,sourcefileurl,sgcn_state,sgcn_year,scientificname_submitted,commonname_submitted,taxonomicgroup_submitted,firstyear,dateinserted) \
         VALUES \
-        ('"+record["sourceid"]+"','"+record["sourcefilename"]+"','"+record["sourcefileurl"]+"','"+record["sgcn_state"]+"',"+str(record["sgcn_year"])+",'"+record["scientificname_submitted"]+"','"+record["commonname_submitted"]+"','"+record["taxonomicgroup_submitted"]+"',"+str(record["firstyear"])+")"
+        ('"+record["sourceid"]+"','"+record["sourcefilename"]+"','"+record["sourcefileurl"]+"','"+record["sgcn_state"]+"',"+str(record["sgcn_year"])+",'"+record["scientificname_submitted"]+"','"+record["commonname_submitted"]+"','"+record["taxonomicgroup_submitted"]+"',"+str(record["firstyear"])+",'"+datetime.utcnow().isoformat()+"')"
     return requests.get(baseURL+"&q="+q).json()
+
+# Get date inserted for SGCN state/year
+def getDateInserted(baseURL,sgcn_state,sgcn_year):
+    import requests
+    r = requests.get(baseURL+"&q=SELECT dateinserted FROM sgcn.sgcn WHERE sgcn_year="+str(sgcn_year)+" AND sgcn_state='"+sgcn_state+"' LIMIT 1").json()
+    if len(r["features"]) > 0:
+        return r["features"][0]["properties"]["dateinserted"]
+    else:
+        return "1992-08-08"
---
+++ bis/worms.py
@@ -0,0 +1,43 @@
+def getWoRMSSearchURL(searchType,target):
+    if searchType == "ExactName":
+        return  "http://www.marinespecies.org/rest/AphiaRecordsByName/"+target+"?like=false&marine_only=false&offset=1"
+    elif searchType == "FuzzyName":
+        return  "http://www.marinespecies.org/rest/AphiaRecordsByName/"+target+"?like=true&marine_only=false&offset=1"
+    elif searchType == "AphiaID":
+        return "http://www.marinespecies.org/rest/AphiaRecordByAphiaID/"+target
+    
+# Pair worms properties that we want to cache
+def packageWoRMSPairs(matchMethod,wormsData):
+    import datetime
+    dt = datetime.datetime.utcnow().isoformat()
+    wormsPairs = '"cacheDate"=>"'+dt+'"'
+    wormsPairs = wormsPairs+',"wormsMatchMethod"=>"'+matchMethod+'"'
+
+    if type(wormsData) is int:
+        return wormsPairs
+    else:
+        wormsPairs = wormsPairs+',"AphiaID"=>"'+str(wormsData['AphiaID'])+'"'
+        wormsPairs = wormsPairs+',"scientificname"=>"'+wormsData['scientificname']+'"'
+        wormsPairs = wormsPairs+',"status"=>"'+wormsData['status']+'"'
+        wormsPairs = wormsPairs+',"rank"=>"'+wormsData['rank']+'"'
+        wormsPairs = wormsPairs+',"valid_name"=>"'+wormsData['valid_name']+'"'
+        wormsPairs = wormsPairs+',"lsid"=>"'+wormsData['lsid']+'"'
+        wormsPairs = wormsPairs+',"isMarine"=>"'+str(wormsData['isMarine'])+'"'
+        wormsPairs = wormsPairs+',"isBrackish"=>"'+str(wormsData['isBrackish'])+'"'
+        wormsPairs = wormsPairs+',"isFreshwater"=>"'+str(wormsData['isFreshwater'])+'"'
+        wormsPairs = wormsPairs+',"isTerrestrial"=>"'+str(wormsData['isTerrestrial'])+'"'
+        wormsPairs = wormsPairs+',"isExtinct"=>"'+str(wormsData['isExtinct'])+'"'
+        wormsPairs = wormsPairs+',"match_type"=>"'+wormsData['match_type']+'"'
+        wormsPairs = wormsPairs+',"modified"=>"'+wormsData['modified']+'"'
+        try:
+            wormsPairs = wormsPairs+',"valid_AphiaID"=>"'+str(wormsData['valid_AphiaID'])+'"'
+            wormsPairs = wormsPairs+',"kingdom"=>"'+wormsData['kingdom']+'"'
+            wormsPairs = wormsPairs+',"phylum"=>"'+wormsData['phylum']+'"'
+            wormsPairs = wormsPairs+',"class"=>"'+wormsData['class']+'"'
+            wormsPairs = wormsPairs+',"order"=>"'+wormsData['order']+'"'
+            wormsPairs = wormsPairs+',"family"=>"'+wormsData['family']+'"'
+            wormsPairs = wormsPairs+',"genus"=>"'+wormsData['genus']+'"'
+        except:
+            pass
+
+        return wormsPairs
