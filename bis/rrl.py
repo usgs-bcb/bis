8347761cc7749a8e506f0acea171b9a5838198a6
Sky Bristol <sbristol@usgs.gov>
2018-05-23 15:36:07 -0600

    New function in rrl on link content negotiation
    I added a function to use HTTP content negotiation on available links
    to retrieve information. So far, this has only been proven to work with
    DOI links, but we may find other cases in future where the link to a
    reference landing page handles content negotiation in some form.

--- bis/rrl.py
+++ bis/rrl.py
@@ -30,34 +30,52 @@ class ResearchReferenceLibrary:
                 result = {"status":"ok","_id":existingRecord["_id"],"message":"Citation string already registered; new source added."}
 
         return result
+
+    
+    def ref_link_data(url):
+        import requests
+        from datetime import datetime
+        
+        response = {"Date Checked":datetime.utcnow().isoformat(),"Link Checked":url}
+        
+        try:
+            response["Link Response"] = requests.get(url, headers={"Accept":"application/json"}).json()
+            response["Success"] = True
+        except:
+            response["Success"] = False
+        
+        return response
+    
     
     def lookup_crossref(citation,doi=None,threshold=60):
         import requests
+        from datetime import datetime
+        
+        crossRefDoc = {"Success":False,"Check Date":datetime.utcnow().isoformat()}
         
         crossRefWorksAPI = "https://api.crossref.org/works"
         mailTo = "bcb@usgs.gov"
         
         crossRefQueryNoDOI = crossRefWorksAPI+"?mailto="+mailTo+"&query.bibliographic="+citation
-        
-        if doi is not None:
+        if doi is None:
+            crossRefQuery = crossRefQueryNoDOI
+        else:
             crossRefQuery = crossRefQueryNoDOI+"&filter=doi:"+doi
-            
+        
+        crossRefDoc["Query URL"] = crossRefQuery
         crossRefResults = requests.get(crossRefQuery).json()
         
-        if crossRefResults["status"] == "failed":
-            return None
-        
-        if doi is not None and "items" in crossRefResults["message"] and len(crossRefResults["message"]["items"]) == 1:
-            return crossRefResults["message"]["items"][0]
-        
-        if doi is not None and "items" in crossRefResults["message"] and len(crossRefResults["message"]["items"]) == 0:
-            crossRefResults = requests.get(crossRefQueryNoDOI).json()
-            
-            if crossRefResults["status"] == "failed":
-                return None
-            
-        if crossRefResults["message"]["items"][0]["score"] >= threshold:
-            return crossRefResults["message"]["items"][0]
+        if "items" in crossRefResults["message"].keys() and len(crossRefResults["message"]["items"]) > 0 and crossRefResults["message"]["items"][0]["score"] >= threshold:
+            crossRefDoc["Success"] = True
+            crossRefDoc["Score"] = crossRefResults["message"]["items"][0]["score"]
+            crossRefDoc["Record"] = crossRefResults["message"]["items"][0]
         else:
-            return None
-            
+            if doi is not None:
+                crossRefDoc["Query URL"] = crossRefQueryNoDOI
+                crossRefResults = requests.get(crossRefQueryNoDOI).json()
+                if "items" in crossRefResults["message"].keys() and len(crossRefResults["message"]["items"]) > 0 and crossRefResults["message"]["items"][0]["score"] >= threshold:
+                    crossRefDoc["Success"] = True
+                    crossRefDoc["Score"] = crossRefResults["message"]["items"][0]["score"]
+                    crossRefDoc["Record"] = crossRefResults["message"]["items"][0]
+
+        return crossRefDoc
