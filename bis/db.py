6e4763e8d5cd552a2adb5bf5497f7ef4b09aedd9
sbristol@usgs.gov <sbristol@usgs.gov>
2018-07-13 14:39:22 -0600

    Changed db connection stuff
    Shifted this branch to use new db connection env variables

---
+++ bis/db.py
@@ -0,0 +1,6 @@
+def getDB(dbname):
+    import os
+    from pymongo import MongoClient
+    mongoURI = "mongodb://"+os.environ["DB_USERNAME"]+":"+os.environ["DB_PASSWORD"]+"@"+os.environ["MONGODB_SERVER"]+"/"+os.environ["DB_DATABASE"]
+    client = MongoClient(mongoURI)
+    return client[dbname]
--- bis/dd.py
+++
@@ -1,7 +0,0 @@
-import os
-mongoURI = "mongodb://"+os.environ["MONGOUSER"]+":"+os.environ["MONGOPASS"]+"@"+os.environ["MONGOSERVER"]+os.environ["MONGOPATH"]
-
-def getDB(dbname):
-    from pymongo import MongoClient
-    client = MongoClient(mongoURI)
-    return client[dbname]
